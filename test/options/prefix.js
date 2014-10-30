4e16ee5446612f2afb46d5a5fb0daef11fed57c2
devinivy <devin@bigroomstudios.com>
2014-10-30 17:25:02 -0400

    added count postfix to populate and find.  added tests.

---
+++ test/options/prefix.js
@@ -0,0 +1,94 @@
+// Load modules
+var Lab = require('lab');
+var Hoek = require('hoek');
+var Path = require('path');
+var Hapi = require('hapi')
+var Async = require('async')
+var ServerSetup = require('../server.setup.js');
+
+// Test shortcuts
+var lab = exports.lab = Lab.script();
+var expect = Lab.expect;
+var before = lab.before;
+var after = lab.after;
+var experiment = lab.experiment;
+var test = lab.test;
+
+
+experiment('Prefix option', function () {
+    
+    // This will be a Hapi server for each test.
+    var server = new Hapi.Server();
+
+    // Setup Hapi server to register the plugin
+    before(function(done){
+        
+        ServerSetup(server, {
+            userModel: 'animals',
+            userIdProperty: 'animal.id',
+            userUrlPrefix: '/animal'
+        }, function(err) {
+            
+            if (err) done(err);
+            
+            server.route([
+            { // find with prefix
+                method: 'GET',
+                path: '/v1/treat',
+                handler: {
+                    bedwetter: {
+                        prefix: '/v1'
+                    }
+                }
+            }]);
+            
+            done();
+        });
+    });
+    
+    test('finds with prefix.', function (done) {
+        
+        server.inject({
+            method: 'GET',
+            url: '/v1/treat?limit=1'
+        }, function(res) {
+            
+            expect(res.statusCode).to.equal(200);
+            expect(res.result).to.be.an.array;
+            // console.log(res.statusCode, res.result);
+            
+            done();
+        })
+        
+    });
+    
+    after(function(done) {
+        
+        var orm = server.plugins.dogwater.zoo.waterline;
+        
+        /* Take each connection used by the orm... */
+        Async.each(Object.keys(orm.connections), function(key, cbDone) {
+            
+            var adapter = orm.connections[key]._adapter;
+            
+            /* ... and use the relevant adapter to kill it. */
+            if (typeof adapter.teardown === "function") {
+                
+                adapter.teardown(function(err) {
+                    cbDone(err);
+                });
+                
+            } else {
+                cbDone();
+            }
+            
+        },
+        function (err) {
+            done(err);
+        });
+        
+    });
+    
+});
+
+
